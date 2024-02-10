e58eaba4ed9b67689051fe3964c8fb6c6c2e064a
Alexey372 <alextm272013@gmail.com>
2024-02-11 03:06:50 +0900

    nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
-import "./globals.css"
+import "@/assets/styles/fonts.css"
+import "@/assets/styles/globals.css"
 
 export const metadata: Metadata = {
   title: "Create Next App",
